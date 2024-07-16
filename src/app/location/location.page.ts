c0ff1cbbbf83d3881a0fc8c6c8024c96d18baca5
KanehiroSW <kanehirosw@gmail.com>
2024-07-15 20:12:55 -0500

    LOCATION

--- src/app/location/location.page.ts
+++ src/app/location/location.page.ts
@@ -24,17 +24,25 @@ export class LocationPage implements AfterViewInit {
 
     const position = await Geolocation.getCurrentPosition();
 
-    this.map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 14);
+    this.map = L.map('map').setView(
+      [position.coords.latitude, position.coords.longitude],
+      14
+    );
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.map);
 
-    this.addMarker(position.coords.latitude, position.coords.longitude, 'Mi ubicación');
+    this.addMarker(
+      position.coords.latitude,
+      position.coords.longitude,
+      'Mi ubicación'
+    );
 
     this.searchHospitals(position.coords.latitude, position.coords.longitude);
 
-    this.mapInitialized = true;  // Set the flag to true after initializing the map
+    this.mapInitialized = true;
   }
 
   addMarker(lat: number, lng: number, title: string) {
@@ -42,7 +50,6 @@ export class LocationPage implements AfterViewInit {
   }
 
   searchHospitals(lat: number, lng: number) {
-    // Utiliza la API de Overpass para buscar hospitales
     const query = `
       [out:json];
       node
@@ -50,16 +57,22 @@ export class LocationPage implements AfterViewInit {
         (around:5000, ${lat}, ${lng});
       out body;
     `;
-    const url = `https://overpass-api.de/api/interpreter?data=${encodeURIComponent(query)}`;
+    const url = `https://overpass-api.de/api/interpreter?data=${encodeURIComponent(
+      query
+    )}`;
 
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         const hospitals = data.elements;
         hospitals.forEach((hospital: any) => {
-          this.addMarker(hospital.lat, hospital.lon, hospital.tags.name || 'Hospital');
+          this.addMarker(
+            hospital.lat,
+            hospital.lon,
+            hospital.tags.name || 'Hospital'
+          );
         });
       })
-      .catch(error => console.error('Error fetching hospitals:', error));
+      .catch((error) => console.error('Error fetching hospitals:', error));
   }
 }
