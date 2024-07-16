787bb8f2c85980505fdecac87ff0ddd2bcef0ab0
JoseCarden <josejesuscardenasjimenez@gmail.com>
2024-07-16 16:58:33 -0500

    Perfil Usuario (incompleto)
    La pagina muestra la información de usuario con id 1 solamente

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioResponse } from 'src/interfaces/intUsuario/UsuarioResponse';
+import { UsuarioService } from '../services/usuario.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+   Usuario: UsuarioResponse = {
+    altura:     0,    apell:     '',
+    direccion: '',    dni:       '',
+    edad:       0,    email:     '',
+    fecha_nac: '',    idUsuario:  0,
+    nombre:    '',    password:  '',
+    peso:       0,    rol:        0,
+    sexo:      '',    telefono:  '',
+    usuario:   ''
+  };
+  User = [];
 
-  ngOnInit() {
+  constructor(
+    private usSrv: UsuarioService
+  ) { }
+
+  async ngOnInit() {
+    // const est = localStorage.getItem('nata')
+    // if(est){
+    //   this.User = JSON.parse(est);
+    // }else{
+    //   this.User = []
+    // }
+
+    const user = await firstValueFrom(this.usSrv.getOneUsuario(1));
+
+    this.Usuario = {
+      altura:     user.altura,       apell:      user.apell,
+      direccion:  user.direccion,    dni:        user.dni,
+      edad:       user.edad,         email:      user.email,
+      fecha_nac:  user.fecha_nac,    idUsuario:  user.idUsuario,
+      nombre:     user.nombre,       password:   user.password,
+      peso:       user.peso,         rol:        user.rol,
+      sexo:       user.sexo,         telefono:   user.telefono,
+      usuario:    user.usuario
+    }
   }
 
 }
